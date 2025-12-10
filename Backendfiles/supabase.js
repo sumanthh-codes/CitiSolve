44dc0443f6072fd8961892b14b3621854978df0e
Ashrith Reddy <tummalaashrithreddy2391@gmail.com>
2025-12-10 10:40:09 +0530

    replacing the required supabase in corect directory

--- Backendfiles/supabase.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config(); // load .env
-
-import { createClient } from '@supabase/supabase-js' ;
-
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
